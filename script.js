10a4e63949d3d01eae8113332badb362bc8bbdf8
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-07-01 17:58:58 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const day = document.querySelector(".day");
+const time = document.querySelector(".time");
+const getCurrentTime_Day = function () {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const now = new Date();
+  time.textContent = now
+    .toUTCString()
+    .split(" ")[4]
+    .split(":")
+    .slice(0, 2)
+    .join(":");
+  day.textContent = days[now.getUTCDay()];
+};
+
+setInterval(getCurrentTime_Day, 1000);
